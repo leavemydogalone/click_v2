e64f8feb9b56ebbafbd074834732116dd2422ea6
leavemydogalone <niklasport88@gmail.com>
2022-07-14 22:04:27 -0700

    form added

--- frontendy/src/components/Login.js
+++ frontendy/src/components/Login.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-      <h3 className="">Hello</h3>
-      <button className="btn">im a button</button>
+      <form className="loginForm">
+        <label htmlFor="username" className="block">
+          Username
+        </label>
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <label htmlFor="password" className="block">
+          Password
+        </label>
+        <input
+          // type="text"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button className="btn block" onClick={() => {}}>
+          Login
+        </button>
+      </form>
     </>
   );
 }
