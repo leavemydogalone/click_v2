d50b7e97c4d8b971c2e3deeda5c9742fc6842de8
leavemydogalone <niklasport88@gmail.com>
2022-07-19 21:34:52 -0700

    the egg has landed

--- frontendy/src/components/Character.js
+++ frontendy/src/components/Character.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Penguin from "../unlockables/character_models/Penguin";
+import { ReactComponent as Egg } from "../unlockables/character_models/Egg.svg";
 
 export default function Character() {
   return (
     <div className="w-1/4">
-      <Penguin />
+      <Egg />
     </div>
   );
 }
--- frontendy/src/components/GameContainer.js
+++ frontendy/src/components/GameContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import TheButton from "./TheButton";
 import Character from "./Character";
+import grass from "../unlockables/backgrounds/layered-waves-haikei.svg";
 
 export default function GameContainer() {
   return (
-    <div className="flex flex-col justify-end items-center py-10">
+    <div
+      className="flex flex-col justify-end items-center py-10 bg-cover bg-no-repeat bg-bottom"
+      style={{ backgroundImage: `url(${grass}` }}
+    >
       <Character />
       <TheButton />
     </div>
